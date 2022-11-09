cdbe1c20bff4e3bc9e327dbc67adb1783f83bc94
vladislavKrosfli <106679280+vladislavKrosfli@users.noreply.github.com>
2022-11-09 18:38:27 +0300

    Add files via upload

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,28 +1,26 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class calculatorTest {
-    calculator calc = new calculator();
+    Calculation calc = new Calculation();
 
     @Test
     public void addTest(){
-        Assert.assertEquals(25,calculator.add(15.0,10.0),0);
+        Assert.assertEquals(25, Calculation.add(15.0,10.0),0);
     }
 
     @Test
     public void subTest(){
-        Assert.assertEquals(5,calculator.sub(15.0,10.0),0);
+        Assert.assertEquals(5, Calculation.sub(15.0,10.0),0);
     }
 
     @Test
     public void mulTest(){
-        Assert.assertEquals(150,calculator.mul(15.0,10.0),0);
+        Assert.assertEquals(150, Calculation.mul(15.0,10.0),0);
     }
 
     @Test
     public void divTest(){
-        Assert.assertEquals(1.5,calculator.div(15.0,10.0),0);
+        Assert.assertEquals(1.5, Calculation.div(15.0,10.0),0);
     }
 }
